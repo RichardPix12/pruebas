696c8623128fe9dd675635a87459ee32f0c565fd
RichardPix12 <richard.marques1234@gmail.com>
2023-02-18 11:52:12 +0100

    pruebas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+const [subs, setSubs] = useState([
+  {
+    nick: 'Dapelu',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    description: 'Dapelu hace de morerador a veces '
+  },
+  {
+    nick: 'Richard',
+    subMonths: 2,
+    avatar: 'https://i.pravatar.cc/150?u=richard',
+  }
+]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>midu subs</h1>
+      <ul>
+        {
+          subs.map(sub => {
+            return(
+              <li key = {sub.nick}>
+                <img src={sub.avatar} alt = {`Avatar for ${sub.nick}`} />
+                <h4>{sub.nick}(<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0,100)}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
